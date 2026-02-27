3e137023a0bca0966da7b490b61591add113e399
Александр Харо <alexkharo@yandex.ru>
2026-02-27 07:01:58 +0300

    Update settings.py

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -118,8 +118,9 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     ],
     'DEFAULT_FILTER_BACKENDS': (
-        'django_filters.rest_framework.SearchFilter',
+        'django_filters.rest_framework.DjangoFilterBackend',
         'rest_framework.filters.SearchFilter',
+        'rest_framework.filters.OrderingFilter',
     ),
 
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
