355cdbcbe1ded077a05344516ce1ca21c5e7bc3e
Александр Харо <alexkharo@yandex.ru>
2026-04-02 02:07:18 +0300

    Update settings.py

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -129,6 +129,10 @@ REST_FRAMEWORK = {
 DJOSER = {
     'LOGIN_FIELD': 'email',
     'HIDE_USERS': False,
+    'TOKEN_MODEL': 'rest_framework.authtoken.models.Token',
+    'PERMISSIONS': {
+        'user_list': ['rest_framework.permissions.AllowAny']
+    },
     'SERIALIZERS': {
         'user': 'api.serializers.UserSerializer',
         'current_user': 'api.serializers.UserSerializer',
